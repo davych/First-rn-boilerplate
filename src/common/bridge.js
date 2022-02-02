926cc12d035fd843201f508aa995c43737554263
davy_chen <davy_chen@epam.com>
2022-02-02 14:45:38 +0800

    add AndroidCallback and Android Class

--- src/common/bridge.js
+++ src/common/bridge.js
@@ -2,7 +2,25 @@ class Bridge {
   injectedJavascript = `(function() {
     window.postMessage = function(data) {
       window.ReactNativeWebView.postMessage(data);
-  };
+    };
+    window.Android = {
+      takePicture: function() {
+        window.postMessage(JSON.stringify({method: 'takePhoto'}));
+      },
+      getCurrentLocation: function() {
+        window.postMessage(JSON.stringify({method: 'getCurrentLocation'}));
+      }
+    }
+
+    window.addEventListener('message', function(event) {
+      const data = JSON.parse(event.data);
+      const AndroidCallback = window.AndroidCallback || {};
+      if (data.method === 'onGetPhotoURI') {
+        window.AndroidCallback.onGetPhotoURI && window.onGetPhotoURI(data.params.uri);
+      } else if (data.method === 'onGetCurrentLocation') {
+        window.AndroidCallback.onGetCurrentLocation && window.onGetCurrentLocation(data.params.location);
+      }
+    });
   })()`;
   setWebViewRef(webView) {
     if (!this.webView) {
